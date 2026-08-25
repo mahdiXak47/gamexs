a5a2f23475c7ca2422a07f19179b4853c03614b4
mahdiXak47 <mahdiamcfc@gmail.com>
2026-08-25 13:27:08 +0330

    fix: django admin problme resolved

--- backend/gamexs/settings/base.py
+++ backend/gamexs/settings/base.py
@@ -92,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "fa-ir"
+LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Tehran"
 USE_I18N = True
 USE_TZ = True
